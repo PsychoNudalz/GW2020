c891da4e8c069ab239570eb110351a0150f1541b
Anson <anson.aw0618@gmail.com>
2020-08-07 22:00:56 +0100

    completed main menu

---
+++ Assets/HelpMenuScript.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HelpMenuScript : MonoBehaviour
+{
+    public GameObject[] pages;
+    public int currentPage = 0;
+    public GameObject nextButton;
+    public GameObject prevButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        checkPage();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void nextPage()
+    {
+        if (currentPage < pages.Length-1)
+        {
+            pages[currentPage].SetActive(false);
+            currentPage++;
+            checkPage();
+            pages[currentPage].SetActive(true);
+            print("next page " + currentPage);
+        }
+    }
+
+    public void prevPage()
+    {
+        if (currentPage != 0)
+        {
+            pages[currentPage].SetActive(false);
+            currentPage--;
+            checkPage();
+            pages[currentPage].SetActive(true);
+            print("prev page " + currentPage);
+
+        }
+    }
+
+    void checkPage()
+    {
+        if (currentPage == 0)
+        {
+            prevButton.SetActive(false);
+        }
+        else
+        {
+            prevButton.SetActive(true);
+        }
+        if (currentPage == pages.Length - 1)
+        {
+            nextButton.SetActive(false);
+        }
+        else
+        {
+            nextButton.SetActive(true);
+        }
+    }
+
+    public void startLevel1()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void closeMenu()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
---
+++ Assets/mainMenuScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Sound theme;
+
+    private void Start()
+    {
+        if (theme != null)
+        {
+            FindObjectOfType<SoundManager>().Play(theme.name);
+
+        }
+    }
+    public void toMainMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+    }
+
+    public void toHelpMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void startLevel1()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    public void startLevel2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
